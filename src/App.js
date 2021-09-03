5edcb11c3067ae53e890b502dc902bbb599f7dfe
ThaddG <thaddious.gorges@gmail.com>
2021-09-03 17:19:05 -0400

    added regular and guarded route example to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
+
+// custom components
+import GuardedRoute from './components/GuardedRoute';
+
+// styles
 import './App.css';
 
 function App() {
   return (
+    // TODO: If you're going to use App as a component switch the use the code below
+    // <Router>
+    //   <Route exact path="/" component={Home} />
+    //   <GuardedRoute
+    //     path="/signup"
+    //     check={isUserNotSignedIn}
+    //     component={Signup}
+    //     redirectTo="/"
+    //   />
+    // </Router>
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
